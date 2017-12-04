9d8a637ce83eaa5f9400d71d829ab81783d4234d
estrattonbailey <ericstrattonbailey@gmail.com>
2017-12-03 21:35:27 -0500

    ⚡︎

--- components/App.js
+++ components/App.js
@@ -7,22 +7,22 @@ module.exports = function App (children = '', css = '') {
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
 
-        <meta name="description" content=""/>
+        <meta name="description" content="Developer. Interested in design systems, open source, and beer. Based in NYC."/>
 
-        <meta property="og:url" content=""/>
-        <meta property="og:site_name" content=""/>
+        <meta property="og:url" content="https://estrattonbailey.com"/>
+        <meta property="og:site_name" content="estrattonbailey"/>
         <meta property="og:type" content="website"/>
-        <meta property="og:title" content=""/>
-        <meta property="og:description" content=""/>
+        <meta property="og:title" content="estrattonbailey"/>
+        <meta property="og:description" content="Developer. Interested in design systems, open source, and beer. Based in NYC."/>
         <meta property="og:image" content=""/>
 
         <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content=""/>
-        <meta name="twitter:url" content=""/>
-        <meta name="twitter:title" content=""/>
-        <meta name="twitter:description" content=""/>
+        <meta name="twitter:site" content="estrattonbailey"/>
+        <meta name="twitter:url" content="https://estrattonbailey@gmail.com"/>
+        <meta name="twitter:title" content="estrattonbailey"/>
+        <meta name="twitter:description" content="Developer. Interested in design systems, open source, and beer. Based in NYC."/>
         <meta name="twitter:image" content=""/>
 
         <link rel='stylesheet' href='https://unpkg.com/svbstrate@4.0.6/dist/svbstrate.css' />
--- index.js
+++ index.js
@@ -6,4 +6,9 @@ const router = require('./router.js')
 app.use(express.static('public'))
 app.use(router)
 
+app.get('/dump', (req, res) => {
+  require('./lib/cache.js').clear()
+  res.status(200).send('Success!')
+})
+
 app.listen(8080, () => console.log('Listening on port 8080'))
